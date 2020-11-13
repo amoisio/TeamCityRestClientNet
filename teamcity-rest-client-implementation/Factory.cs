42e48b472d542108a6f40b412b202ce9317e243f
Aleksi Moisio <aleksi.moisio30@gmail.com>
2020-11-13 23:43:31 +0200

    Port Factory.cs and add a stub implementation of TeamCityInstanceImpl.

--- teamcity-rest-client-implementation/Factory.cs
+++ teamcity-rest-client-implementation/Factory.cs
@@ -1,43 +1,59 @@
+using System;
+using System.Text;
+
 namespace TeamCityRestClientNet
 {
     /**
- * Factory object to create new object of [TeamCityInstance] interface
- *
- * @see TeamCityInstance
- */
-// public static class TeamCityInstanceFactory {
-//   /**
-//    * Creates guest authenticated accessor
-//    * @param serverUrl HTTP or HTTPS URL to TeamCity server
-//    *
-//    * Used via reflection for backward compatibility for deprecated methods
-//    */
-//   public static TeamCityInstance guestAuth(string serverUrl) 
-//     => createGuestAuthInstance(serverUrl);
+  * Factory object to create new object of [TeamCityInstance] interface
+  *
+  * @see TeamCityInstance
+  */
+    public static class TeamCityInstanceFactory
+    {
+        /**
+        * Creates guest authenticated accessor
+        * @param serverUrl HTTP or HTTPS URL to TeamCity server
+        *
+        * Used via reflection for backward compatibility for deprecated methods
+        */
+        public static TeamCityInstance guestAuth(string serverUrl)
+          => createGuestAuthInstance(serverUrl);
 
-//   /**
-//    * Creates username/password authenticated accessor
-//    *
-//    * @param serverUrl HTTP or HTTPS URL to TeamCity server
-//    * @param username username
-//    * @param password password
-//    *
-//    * Used via reflection for backward compatibility for deprecated methods
-//    */
-//   public static TeamCityInstance httpAuth(string serverUrl, string username, string password)
-//     => createHttpAuthInstance(serverUrl, username, password);
+        internal static TeamCityInstanceImpl createGuestAuthInstance(string serverUrl)
+          => new TeamCityInstanceImpl(serverUrl.TrimEnd('/'), "/guestAuth", null, false);
 
-//   /**
-//    * Creates token based connection.
-//    * TeamCity access token generated on My Settings & Tools | Access Tokens
-//    *
-//    * @param serverUrl HTTP or HTTPS URL to TeamCity server
-//    * @param token token
-//    *
-//    * see https://www.jetbrains.com/help/teamcity/rest-api.html#RESTAPI-RESTAuthentication
-//    */
-//   public static TeamCityInstance tokenAuth(string serverUrl, string token)
-//     => createTokenAuthInstance(serverUrl, token);
-// }
+        /**
+        * Creates username/password authenticated accessor
+        *
+        * @param serverUrl HTTP or HTTPS URL to TeamCity server
+        * @param username username
+        * @param password password
+        *
+        * Used via reflection for backward compatibility for deprecated methods
+        */
+        public static TeamCityInstance httpAuth(string serverUrl, string username, string password)
+          => createHttpAuthInstance(serverUrl, username, password);
 
+        internal static TeamCityInstanceImpl createHttpAuthInstance(string serverUrl, string username, string password)
+        {
+            var bytes = Encoding.UTF8.GetBytes($"{username}:{password}");
+            var authorization = Convert.ToBase64String(bytes);
+            return new TeamCityInstanceImpl(serverUrl.TrimEnd('/'), "/httpAuth", "Basic $authorization", false);
+        }
+
+        /**
+        * Creates token based connection.
+        * TeamCity access token generated on My Settings & Tools | Access Tokens
+        *
+        * @param serverUrl HTTP or HTTPS URL to TeamCity server
+        * @param token token
+        *
+        * see https://www.jetbrains.com/help/teamcity/rest-api.html#RESTAPI-RESTAuthentication
+        */
+        public static TeamCityInstance tokenAuth(string serverUrl, string token)
+          => createTokenAuthInstance(serverUrl, token);
+
+        internal static TeamCityInstanceImpl createTokenAuthInstance(string serverUrl, string token)
+          => new TeamCityInstanceImpl(serverUrl.TrimEnd('/'), "", "Bearer $token", false);
+    }
 }
