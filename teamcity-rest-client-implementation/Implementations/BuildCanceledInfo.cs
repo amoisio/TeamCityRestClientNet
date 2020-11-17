1b96a45eda994a186696c9bccadf4162dde47f52
Aleksi Moisio <aleksi.moisio30@gmail.com>
2020-11-17 18:57:10 +0200

    Implement BuildCancelInfo.

---
+++ teamcity-rest-client-implementation/Implementations/BuildCanceledInfo.cs
@@ -0,0 +1,30 @@
+using System;
+using TeamCityRestClientNet.Api;
+using TeamCityRestClientNet.Service;
+using TeamCityRestClientNet.Tools;
+
+namespace TeamCityRestClientNet.Implementations
+{
+    internal class BuildCanceledInfo : IBuildCanceledInfo
+    {
+        private readonly BuildCanceledDto _dto;
+        private readonly TeamCityInstance _instance;
+
+        internal BuildCanceledInfo(BuildCanceledDto dto, TeamCityInstance instance)
+        {
+            this._dto = dto;
+            this._instance = instance;
+        }
+
+        public IUser User
+         => _dto.User != null
+            ? new User(_dto.User, false, _instance)
+            : null;
+
+        public DateTimeOffset CancelDateTime
+            => Utilities.ParseTeamCity(_dto.Timestamp)
+            ?? throw new NullReferenceException();
+
+        public string Text => _dto.Text ?? String.Empty;
+    }
+}
