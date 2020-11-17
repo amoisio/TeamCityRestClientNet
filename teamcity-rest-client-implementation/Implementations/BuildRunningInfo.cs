4c9163ca19d90d5d0b6dfe072ccc2050f0171527
Aleksi Moisio <aleksi.moisio30@gmail.com>
2020-11-17 23:02:22 +0200

    Implement BuildRunningInfo.

---
+++ teamcity-rest-client-implementation/Implementations/BuildRunningInfo.cs
@@ -0,0 +1,21 @@
+using TeamCityRestClientNet.Api;
+using TeamCityRestClientNet.Service;
+
+namespace TeamCityRestClientNet.Implementations 
+{
+    class BuildRunningInfo : IBuildRunningInfo
+    {
+        private readonly BuildRunningInfoDto _dto;
+
+        public BuildRunningInfo(BuildRunningInfoDto dto)
+        {
+            this._dto = dto;
+        }
+
+        public int PercentageComplete => this._dto.PercentageComplete;
+        public long ElapsedSeconds => this._dto.ElapsedSeconds;
+        public long EstimatedTotalSeconds => this._dto.EstimatedTotalSeconds;
+        public bool Outdated => this._dto.Outdated;
+        public bool ProbablyHanging => this._dto.ProbablyHanging;
+    }
+}
