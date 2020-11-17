fbc63d53c1f59cafc72ae50a63e0a3c9a41e4ecd
Aleksi Moisio <aleksi.moisio30@gmail.com>
2020-11-17 23:04:29 +0200

    Add ParseTeamCity utility.

--- teamcity-rest-client-implementation/Utilities.cs
+++ teamcity-rest-client-implementation/Utilities.cs
@@ -1,11 +1,37 @@
+using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
+using TeamCityRestClientNet.Implementations;
 
-namespace TeamCityRestClientNet.Utilities
+namespace TeamCityRestClientNet.Tools
 {
-    public static class ListUtilities
+    public static class Utilities
     {
         public static List<T> ListOfNotNull<T>(params T[] values)
             => values.Where(val => val != null).ToList();
+
+
+        public static DateTimeOffset? ParseTeamCity(string teamCityDateTime) 
+        {
+            if (String.IsNullOrEmpty(teamCityDateTime))
+                return null;
+            else
+                return DateTimeOffset.ParseExact(
+                    teamCityDateTime, 
+                    TeamCityInstance.TEAMCITY_DATETIME_FORMAT, 
+                    CultureInfo.CurrentCulture);
+        }
+
+
+        // private fun SaveToFile(response: retrofit.client.Response, file: File) {
+        //     file.parentFile.mkdirs()
+        //     val input = response.body.`in`()
+        //     BufferedOutputStream(FileOutputStream(file)).use {
+        //         input.copyTo(it)
+        //     }
+        // }
     }
+
+    
 }
