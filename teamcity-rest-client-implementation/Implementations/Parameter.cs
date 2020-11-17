352b265e8868d21cfee16416b2d529ea216bcf1e
Aleksi Moisio <aleksi.moisio30@gmail.com>
2020-11-17 22:47:43 +0200

    Implement Parameter.

---
+++ teamcity-rest-client-implementation/Implementations/Parameter.cs
@@ -0,0 +1,27 @@
+using System;
+using TeamCityRestClientNet.Api;
+using TeamCityRestClientNet.Service;
+
+namespace TeamCityRestClientNet.Implementations
+{
+    class Parameter : IParameter {
+        private readonly ParameterDto _dto;
+
+        public Parameter(ParameterDto dto)
+        {
+            this._dto = dto;
+        }
+
+        public string Name 
+            => String.IsNullOrEmpty(this._dto.Name)
+                ? throw new NullReferenceException()
+                : this._dto.Name;
+
+        public string Value
+            => String.IsNullOrEmpty(this._dto.Value)
+                ? throw new NullReferenceException()
+                : this._dto.Value;
+
+        public bool Own => this._dto.Own ?? throw new NullReferenceException();
+    }
+}
