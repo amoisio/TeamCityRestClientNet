f6da55811f230181afeff2897dda330e00b9dfe7
Aleksi Moisio <aleksi.moisio30@gmail.com>
2020-11-17 18:55:23 +0200

    Implement User.

---
+++ teamcity-rest-client-implementation/Implementations/User.cs
@@ -0,0 +1,30 @@
+using System.Threading.Tasks;
+using TeamCityRestClientNet.Api;
+using TeamCityRestClientNet.Service;
+
+namespace TeamCityRestClientNet.Implementations
+{
+    class User : Base<UserDto>, IUser
+    {
+        internal User(UserDto dto, bool isFullBuildDto, TeamCityInstance instance)
+            : base(dto, isFullBuildDto, instance)
+        {
+            
+        }
+
+        public UserId Id => new UserId(IdString);
+        public string Username => Dto.Username;
+        public string Name => Dto.Name;
+        public string Email => Dto.Email;
+        public string GetHomeUrl()
+            => Instance.GetUserUrlPage("admin/editUser.html", userId: Id);
+
+        public override string ToString()
+            => $"User(id={Id.stringId}, username={Username})";
+
+        protected override async Task<UserDto> FetchFullDto()
+        {
+            return await Service.Users($"id:{Id.stringId}");
+        }
+    }
+}
