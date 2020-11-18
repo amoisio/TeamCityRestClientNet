436ebdc4475166e197287cdb9257dadc7750c503
Aleksi Moisio <aleksi.moisio30@gmail.com>
2020-11-18 17:29:40 +0200

    Implement VcsRootInstance.

---
+++ teamcity-rest-client-implementation/Implementations/VcsRootInstance.cs
@@ -0,0 +1,24 @@
+using TeamCityRestClientNet.Api;
+using TeamCityRestClientNet.Service;
+using TeamCityRestClientNet.Extensions;
+
+namespace TeamCityRestClientNet.Implementations
+{
+    class VcsRootInstance : IVcsRootInstance
+    {
+        private readonly VcsRootInstanceDto _dto;
+
+        public VcsRootInstance(VcsRootInstanceDto dto)
+        {
+            this._dto = dto;
+        }
+
+        public VcsRootId VcsRootId 
+            => new VcsRootId(this._dto.VcsRootId.SelfOrNullRefException());
+
+        public string Name => this._dto.Name.SelfOrNullRefException();
+
+        public override string ToString()
+            => $"VcsRootInstanceImpl(id={VcsRootId}, name={Name})";
+    }
+}
