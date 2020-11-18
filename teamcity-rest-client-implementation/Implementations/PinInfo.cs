e813040a4fc7cbe5c9a37a6dde797deb08d225fc
Aleksi Moisio <aleksi.moisio30@gmail.com>
2020-11-18 18:30:48 +0200

    Implement PinInfo.

---
+++ teamcity-rest-client-implementation/Implementations/PinInfo.cs
@@ -0,0 +1,26 @@
+using System;
+using TeamCityRestClientNet.Api;
+using TeamCityRestClientNet.Service;
+using TeamCityRestClientNet.Extensions;
+using TeamCityRestClientNet.Tools;
+
+namespace TeamCityRestClientNet.Implementations
+{
+    class PinInfo : IPinInfo
+    {
+        private readonly PinInfoDto _dto;
+        private readonly TeamCityInstance _instance;
+
+        public PinInfo(PinInfoDto dto, TeamCityInstance instance)
+        {
+            this._dto = dto;
+            this._instance = instance;
+        }
+
+        public IUser User 
+            => new User(this._dto.User.SelfOrNullRefException(), false, this._instance);
+
+        public DateTimeOffset DateTime 
+            => Utilities.ParseTeamCity(this._dto.Timestamp.SelfOrNullRefException()).Value;
+    }
+}
