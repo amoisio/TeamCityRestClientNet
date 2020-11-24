f6ee239d1b23d49242b2747e40b767672928ff34
Aleksi Moisio <aleksi.moisio30@gmail.com>
2020-11-24 07:22:51 +0200

    Add BuildProblem and BuildProblemOccurrence implementations.

---
+++ teamcity-rest-client-implementation/Domain/BuildProblem.cs
@@ -0,0 +1,22 @@
+using TeamCityRestClientNet.Api;
+using TeamCityRestClientNet.Service;
+
+namespace TeamCityRestClientNet.Domain
+{
+    class BuildProblem : IBuildProblem
+    {
+        private readonly BuildProblemDto _dto;
+
+        public BuildProblem(BuildProblemDto dto)
+        {
+            _dto = dto;
+        }
+
+        public BuildProblemId Id => new BuildProblemId(_dto.Id);
+        public BuildProblemType Type => new BuildProblemType(_dto.Type);
+        public string Identity => _dto.Identity;
+
+        public override string ToString()
+            => $"BuildProblem(id={Id.stringId},type={Type},identity={Identity})";
+    }
+}
---
+++ teamcity-rest-client-implementation/Domain/BuildProblemOccurrence.cs
@@ -0,0 +1,28 @@
+using Nito.AsyncEx;
+using TeamCityRestClientNet.Api;
+using TeamCityRestClientNet.Service;
+
+namespace TeamCityRestClientNet.Domain
+{
+    class BuildProblemOccurrence : IBuildProblemOccurrence
+    {
+        private readonly BuildProblemOccurrenceDto _dto;
+        private readonly TeamCityInstance _instance;
+
+        public BuildProblemOccurrence(BuildProblemOccurrenceDto dto, TeamCityInstance instance)
+        {
+            this.Build = new AsyncLazy<IBuild>(async () 
+                => await Domain.Build.Create(dto.Build.Id, instance).ConfigureAwait(false));
+            this._dto = dto;
+            this._instance = instance;
+        }
+
+        public IBuildProblem BuildProblem => new BuildProblem(_dto.Problem);
+        public AsyncLazy<IBuild> Build { get; }
+        public string Details => _dto.Details ?? string.Empty;
+        public string AdditionalData => _dto.AdditionalData;
+
+        public override string ToString()
+            => $"BuildProblemOccurrence(build={_dto.Build.Id},problem={BuildProblem},details={Details},additionalData={AdditionalData})";
+    }
+}
