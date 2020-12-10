5e6f68988e63fc487b9c5f8e31ce3ac36e73e856
Aleksi Moisio <aleksi.moisio30@gmail.com>
2020-12-10 22:37:03 +0200

    Clean up interface.

--- TeamCityRestClientNet.Implementation/Authentication/ITeamCityAuthService.cs
+++ TeamCityRestClientNet.Implementation/Authentication/ITeamCityAuthService.cs
@@ -1,4 +1,3 @@
-using System.IO;
 using System.Threading.Tasks;
 using Refit;
 
@@ -6,10 +5,8 @@ namespace TeamCityRestClientNet.Authentication
 {
     public interface ITeamCityAuthService
     {
-        //         To obtain a security token, send the GET https://your-server/authenticationTest.html?csrf request.
-        // To pass the token, use the X-TC-CSRF-Token HTTP request header or the tc-csrf-token HTTP parameter.
-        [Headers("Accept: application/json")]
-        [Get("authenticationTest.html?csrf")]
+        [Headers("Accept: */*")]
+        [Get("/authenticationTest.html?csrf=true")]
         Task<string> CSRFToken();
     }
 }
