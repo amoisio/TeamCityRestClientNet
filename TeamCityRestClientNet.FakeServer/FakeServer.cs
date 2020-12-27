c4560f0a0a2ca1e77dea311ad29a8b38a0875db9
Aleksi Moisio <aleksi.moisio30@gmail.com>
2020-12-27 11:42:48 +0200

    Move user data and methods to user repository.

--- TeamCityRestClientNet.FakeServer/FakeServer.cs
+++ TeamCityRestClientNet.FakeServer/FakeServer.cs
@@ -8,29 +8,33 @@ namespace TeamCityRestClientNet.FakeServer
 {
     public class FakeServer
     {
-        public object ResolveApiCall(ApiCall apiCall)
+        public FakeServer()
         {
-            if (apiCall.Resource == "users")
-            {
-                if (!apiCall.HasLocators)
-                    return this.Users();
-                else if (apiCall.Locators.ContainsKey("id"))
-                    return this.UserById(apiCall.Locators["id"]);
-                else
-                    return this.UserByUsername(apiCall.Locators["username"]);
-            }
-            throw new NotImplementedException();
+            Users = new UserRepository();
         }
 
-        private List<UserDto> _users = new List<UserDto>
+        public UserRepository Users { get; }
+
+        public object ResolveApiCall(ApiCall apiCall)
         {
-            new UserDto { Id = "1", Name = "John Doe", Username = "jodoe", Email = "john.doe@mailinator.com" },
-            new UserDto { Id = "2", Name = "Jane Doe", Username = "jadoe", Email = "jane.doe@mailinator.com" },
-            new UserDto { Id = "3", Name = "Dunkin' Donuts", Username = "dunkin", Email = "dunkin@mailinator.com" },
-            new UserDto { Id = "4", Name = "Mac Cheese", Username = "maccheese", Email = "maccheese@mailinator.com" }
-        };
-        private UserDto UserById(string id) => _users.SingleOrDefault(u => u.Id == id);
-        private UserDto UserByUsername(string username) => _users.SingleOrDefault(u => u.Username == username);
-        private UserListDto Users() => new UserListDto { User = _users };
+            object response = apiCall.Resource switch
+            {
+                "users" => ResolveUsers(apiCall),
+                _       => throw new NotImplementedException()
+            };
+            return response;
+        }
+
+        private object ResolveUsers(ApiCall apiCall)
+        {
+            if (!apiCall.HasLocators)
+                return this.Users.All();
+            else if (apiCall.Locators.ContainsKey("id"))
+                return this.Users.ById(apiCall.Locators["id"]);
+            else
+                return this.Users.ByUsername(apiCall.Locators["username"]);
+        }
+
+        
     }
 }
