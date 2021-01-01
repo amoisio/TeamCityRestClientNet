3d445d1c7ea75fca9128b7c3b1c44cfe1855e234
Aleksi Moisio <aleksi.moisio30@gmail.com>
2021-01-01 23:17:36 +0200

    Reorder methods.

--- TeamCityRestClientNet.Api/TeamCity.cs
+++ TeamCityRestClientNet.Api/TeamCity.cs
@@ -11,16 +11,16 @@ namespace TeamCityRestClientNet.Api
     {
         public abstract Task<IBuild> Build(BuildId id);
         public abstract Task<IBuild> Build(BuildConfigurationId buildConfigurationId, string number);
+        public abstract IBuildLocator Builds { get; }
         public abstract IBuildAgentLocator BuildAgents { get; }
         public abstract IBuildAgentPoolLocator BuildAgentPools { get; }
         public abstract Task<IBuildConfiguration> BuildConfiguration(string id);
         public abstract Task<IBuildConfiguration> BuildConfiguration(BuildConfigurationId id);
         public abstract IBuildQueue BuildQueue { get; }
-        public abstract IBuildLocator Builds { get; }
+        public abstract IAsyncEnumerable<IBuild> QueuedBuilds(ProjectId projectId);
         public abstract IChangeLocator Changes { get; }
         public abstract IInvestigationLocator Investigations { get; }
         public abstract Task<IProject> Project(ProjectId id);
-        public abstract IAsyncEnumerable<IBuild> QueuedBuilds(ProjectId projectId);
         public abstract Task<IProject> RootProject();
         public abstract ITestRunsLocator TestRuns { get; }
         public abstract IUserLocator Users { get; }
