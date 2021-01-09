d3a3e46881e4d6872f8125d2a88609f0f24db721
Aleksi Moisio <aleksi.moisio30@gmail.com>
2021-01-09 10:02:37 +0200

    Revert "Refactor agent pools tests. Comment out tests which are better tested with e2e tests."
    This reverts commit 5a9cb900e38a28cf1730e4b784871e08e1be20d4.

--- TeamCityRestClientNet.Tests/BuildAgentPoolTests.cs
+++ TeamCityRestClientNet.Tests/BuildAgentPoolTests.cs
@@ -5,55 +5,57 @@ using Xunit;
 using TeamCityRestClientNet.Api;
 using TeamCityRestClientNet.Tests;
 using System.Net.Http;
+using TeamCityRestClientNet.FakeServer;
 
 namespace TeamCityRestClientNet.BuildAgentPools
 {
-    public class BuildAgentPools : TestsBase
+    public class BuildAgentPoolList : TestsBase
     {
-        // [Fact]
-        // public async Task Contains_all_build_agent_pools()
-        // {
-        //     var agentPools = await _teamCity.BuildAgentPools.All().ToListAsync();
-        //     Assert.Collection(agentPools, 
-        //         (pool) => Assert.Equal("Default", pool.Name));
-        // }
+        [Obsolete]
+        [Fact]
+        public async Task Contains_all_build_agent_pools()
+        {
+            var agentPools = await _teamCity.BuildAgentPools.All().ToListAsync();
+            Assert.Collection(agentPools, 
+                (pool) => Assert.Equal("Default", pool.Name));
+        }
 
         [Fact]
-        public async Task Can_be_retrieved_by_GETtin_the_agentPools_end_point()
+        public async Task GETs_the_agent_pools_end_point()
         {
             await _teamCity.BuildAgentPools.All().ToListAsync();
 
-            ApiCall2
-                .ExpectedRequest(HttpMethod.Get, "/app/rest/agentPools")
-                .Assert();
+            var apiCall = ApiCall(HttpMethod.Get, "/app/rest/agentPools");
+            Assert.NotNull(apiCall);
         }
     }
 
     public class ExistingBuildAgentPool : TestsBase
     {
-        // [Fact]
-        // public async Task Can_be_retrieved()
-        // {
-        //     var agentPool = await _teamCity.BuildAgentPools.ById(new Id("0"));
+        [Obsolete]
+        [Fact]
+        public async Task Can_be_retrieved()
+        {
+            var agentPool = await _teamCity.BuildAgentPools.ById(new Id("0"));
 
-        //     var agents = await agentPool.Agents;
-        //     Assert.NotEmpty(agents);
-        //     Assert.Contains(agents, a => a.Name == "ip_172.17.0.3");
+            var agents = await agentPool.Agents;
+            Assert.NotEmpty(agents);
+            Assert.Contains(agents, a => a.Name == "ip_172.17.0.3");
 
-        //     var projects = await agentPool.Projects;
-        //     Assert.NotEmpty(projects);
-        //     Assert.Contains(projects, a => a.Id.StringId == "TeamCityCliNet");
-        //     Assert.Contains(projects, a => a.Id.StringId == "TeamCityRestClientNet");
-        // }
+            var projects = await agentPool.Projects;
+            Assert.NotEmpty(projects);
+            Assert.Contains(projects, a => a.Id.StringId == "TeamCityCliNet");
+            Assert.Contains(projects, a => a.Id.StringId == "TeamCityRestClientNet");
+        }
 
         [Fact]
-        public async Task Can_be_retrieved_by_GETtin_the_agentPools_end_point_with_id()
+        public async Task GETs_the_agent_pools_end_point_with_id()
         {
             var agent = await _teamCity.BuildAgentPools.ById(new Id("0"));
 
-            ApiCall2
-                .ExpectedRequest(HttpMethod.Get, "/app/rest/agentPools/id:0")
-                .Assert();
+            var apiCall = ApiCall(HttpMethod.Get, "/app/rest/agentPools/id:0");
+            Assert.NotNull(apiCall);
+            Assert.Equal("0", apiCall.GetLocatorValue());
         }
 
         [Fact]
--- TeamCityRestClientNet.Tests/TestsBase.cs
+++ TeamCityRestClientNet.Tests/TestsBase.cs
@@ -4,25 +4,9 @@ using System.Linq;
 using System.Net.Http;
 using TeamCityRestClientNet.Api;
 using TeamCityRestClientNet.FakeServer;
-using System.Linq.Expressions;
 
 namespace TeamCityRestClientNet.Tests
 {
-    public static class ApiCallExtensions
-    {
-        public static void Assert(this ApiCall apiCall, HttpMethod expectedMethod)
-            => Xunit.Assert.Equal(expectedMethod, apiCall.Method);
-
-        public static void Assert(this ApiCall apiCall, string expectedRequestPath)
-            => Xunit.Assert.Equal(expectedRequestPath, apiCall.RequestPath);
-
-        public static void Assert(this ApiCall apiCall, HttpMethod expectedMethod, string expectedRequestPath)
-        {
-            Assert(apiCall, expectedMethod);
-            Assert(apiCall, expectedRequestPath);
-        }
-    }
-
     public abstract class TestsBase
     {
         protected readonly TeamCityFixture _fixture;
@@ -30,7 +14,13 @@ namespace TeamCityRestClientNet.Tests
         protected readonly string _serverUrl;
         protected readonly RedirectToFakeServer _handler;
         private List<ApiCall> ApiCalls => _handler.ApiCalls;
-
+        protected ApiCall ApiCall(HttpMethod method, string requestPath, Func<ApiCall, bool> filter = null)
+        {
+            var calls = ApiCalls.Where(call => call.Method == method && call.RequestPath == requestPath);
+            if (filter != null)
+                calls = calls.Where(filter);
+            return calls.SingleOrDefault();
+        }
 
         public TestsBase() : this(new TeamCityFixture()) { }
 
@@ -40,86 +30,6 @@ namespace TeamCityRestClientNet.Tests
             _teamCity = fixture.TeamCity;
             _serverUrl = fixture.serverUrl;
             _handler = fixture.Handler;
-        }
-
-
-        internal ApiCallAssert ApiCall2 => new ApiCallAssert(ApiCalls);
-
-        private void AssertApiCall(Predicate<ApiCall> predicate) => Xunit.Assert.Contains(ApiCalls, predicate);
-        protected ApiCall ApiCall(HttpMethod method, string requestPath, Func<ApiCall, bool> filter = null)
-        {
-            var calls = ApiCalls.Where(call => call.Method == method && call.RequestPath == requestPath);
-            if (filter != null)
-                calls = calls.Where(filter);
-            return calls.SingleOrDefault();
-        }
-    }
-
-    class ApiCallAssert
-    {
-        private readonly List<ApiCall> _apiCalls;
-        private HttpMethod _method;
-        private string _requestPath;
-        private bool _matchAny = false;
-
-        public ApiCallAssert(List<ApiCall> apiCalls)
-        {
-            this._apiCalls = apiCalls;
-        }
-
-        public ApiCallAssert MatchAny()
-        {
-            _matchAny = true;
-            return this;
-        }
-
-        public ApiCallAssert ExpectedRequest(HttpMethod method, string requestPath)
-        {
-            _method = method;
-            _requestPath = requestPath;
-            return this;
-        }
-        
-
-        public void Assert()
-        {
-            ParameterExpression pe = Expression.Parameter(typeof(ApiCall), "apiCall");
-
-            List<Expression> expressions = new List<Expression>();
-
-            if (_method != null)
-            {
-                // apiCall.Method == _method
-                Expression left = Expression.Property(pe, typeof(ApiCall).GetProperty("Method"));
-                Expression right = Expression.Constant(_method, typeof(HttpMethod));
-                expressions.Add(Expression.Equal(left, right));
-            }
-
-            if (_requestPath != null)
-            {
-                // apiCall.RequestPath == _requestPath
-                Expression left = Expression.Property(pe, typeof(ApiCall).GetProperty("RequestPath"));
-                Expression right = Expression.Constant(_requestPath, typeof(string));
-                expressions.Add(Expression.Equal(left, right));
-            }
-
-            if (expressions.Count == 0)
-            {
-                throw new InvalidOperationException("At least one condition must be given.");
-            }
-
-            // (apiCall.Method == _method && apiCall.RequestPath == _requestPath)
-            Expression predicateBody = expressions.Count == 1
-                ? expressions[0]
-                : expressions.Aggregate((e1, e2) => Expression.And(e1, e2));
-
-            var le = Expression.Lambda<Predicate<ApiCall>>(predicateBody, new ParameterExpression[] { pe });
-            var predicate = le.Compile();
-
-            if (_matchAny)
-                Xunit.Assert.Contains(_apiCalls, predicate);
-            else
-                Xunit.Assert.Single(_apiCalls, predicate);
         }
     }
 }
